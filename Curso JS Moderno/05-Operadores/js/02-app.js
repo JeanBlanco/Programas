3f7cf5671c6a78abcffb5a00b04bcef306abe4cf
JeanBlanco <107874842+JeanBlanco@users.noreply.github.com>
2022-11-29 16:44:09 -0500

    update

---
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -0,0 +1,32 @@
+const numero1 = 20;
+const numero2 = '20';
+const numero3 = 30;
+
+// revisar si dos valor es igual
+console.log(numero1==numero2);
+console.log(numero1==numero3);
+
+// metodo estricto
+console.log(numero1===numero2);
+console.log(numero1=== parseInt(numero2));
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+// diferente 
+const password1 = 'admi';
+const password2 = 'Admi';
+
+console.log(password1 != password2);
+console.log(numero1!=numero2);
+console.log(numero1!==numero2);
+console.log(numero1!== parseInt(numero2));
+
+
+
+
+
+
+
+
+
+
---
+++ Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,15 @@
+// undefined
+let numero;
+
+console.log(numero);
+console.log(typeof numero);
+
+
+// null
+let nuemro2;
+nuemro2 = null;
+console.log(nuemro2)
+console.log(typeof nuemro2);
+
+console.log(numero == nuemro2);
+console.log(numero===nuemro2);
