df051c6c010e62b60b38af86a78c7f67339ac22a
JeanBlanco <107874842+JeanBlanco@users.noreply.github.com>
2022-12-04 16:46:01 -0500

    Update

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autenticado = true;
+if (autenticado){
+    console.log('Si puedes ver Netflix');
+}else{
+    console.log('No, no puedes ver  netflix');
+}
+
+// Operador ternario
+const Usuario = false;
+console.log(Usuario ? 'si eres usuario' : 'No eres usuario');
